20f94336e67daf1d6672c212f2428f4d550f51e7
PaulKroner <paulfriedrich.kroener@gmail.com>
2022-08-29 14:20:42 +0200

    weitere Änderungen

--- Portfolio Version 2.0/index.js
+++ Portfolio Version 2.0/index.js
@@ -109,3 +109,22 @@ const sidebar = document.querySelector('.sidebar'),
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
+
+// fade in Animation
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
